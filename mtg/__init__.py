34313e48c9ccb2dadcaf1a9414d945677db7fdbe
Darafei Praliaskouski <me@komzpa.net>
2026-04-05 18:56:00 +0400

    Make mtg package import lazy for tests

--- mtg/__init__.py
+++ mtg/__init__.py
@@ -1,2 +1,11 @@
-""" Meshtastic Telegram Gateway package initializer. """
-from .mesh import cmd
+"""Meshtastic Telegram Gateway package initializer."""
+
+from typing import Any
+
+
+def cmd(*args: Any, **kwargs: Any):
+    """Lazily import the CLI entrypoint to avoid eager app dependencies."""
+
+    from .mesh import cmd as mesh_cmd
+
+    return mesh_cmd(*args, **kwargs)
